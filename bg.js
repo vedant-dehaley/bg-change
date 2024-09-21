ace6bead13705bf0b2c38a3428333ce00325129e
vedant-dehaley <159027985+vedant-dehaley@users.noreply.github.com>
2024-09-21 13:39:47 +0530

    Add files via upload
    bg-change

---
+++ bg.js
@@ -0,0 +1,9 @@
+function change1(){
+    const newBg = document.getElementById("highway");
+    document.body.style.backgroundImage = 'url("highway.jpeg")';
+}
+
+function change2(){
+    const newBg = document.getElementById("town");
+    document.body.style.backgroundImage = 'url("town.jpeg")';
+}
